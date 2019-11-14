aaa56823cf657fd13d698cf5e025f9f82e2051d3
onno204 <15333546+onno204@users.noreply.github.com>
2019-11-15 00:43:54 +0100

    fixed all errors

--- main server/src/server/notifications.rs
+++ main server/src/server/notifications.rs
@@ -1,29 +1,25 @@
 extern crate reqwest;
 extern crate serde_json;
 use std::collections::HashMap;
-use reqwest::Error;
-use serde_json::{Result, Value};
 
-
-pub fn send_test_message() -> Result<(), Error> {
-    let do_steps = || -> Result<(), Error> {
-        let request_url = format!("https://api.pushover.net/1/messages.json");
-        let mut params = HashMap::new();
-        params.insert("token", "afip8fu96zoeaikx9oc4obzi6hzb5e");
-        params.insert("user", "u7bmikzkj575u6symxpxjhoueij162");
-        params.insert("message", "YEET");
-        params.insert("priority", "1");
-        let client = reqwest::Client::new();
-        let mut _response = client.post(&request_url)
-            .form(&params)
-            .send()?;
-        let v: Value = serde_json::from_str(&_response.text()?.to_string());
-        // println!("STATUS: {}", response["status"]);
-        Ok(())
-    };
-
-    if let Err(_err) = do_steps() {
-        println!("Failed to perform necessary steps");
-    }
+pub fn send_test_message() -> Result<(), reqwest::Error> {
+    let request_url = format!("https://api.pushover.net/1/messages.json");
+    let mut params = HashMap::new();
+    params.insert("token", "afip8fu96zoeaikx9oc4obzi6hzb5e");
+    params.insert("user", "u7bmikzkj575u6symxpxjhoueij162");
+    params.insert("message", "YEET");
+    params.insert("priority", "1");
+    let client = reqwest::Client::new();
+    let mut _response = client.post(&request_url)
+        .form(&params)
+        .send()?;
+    let output: serde_json::Value = get_string_from_json(&_response.text()?).unwrap();
+    println!("STATUS: {}", output["status"]);
     Ok(())
+
+}
+
+fn get_string_from_json(input_string: &str) -> Result<serde_json::Value, ()> {
+    let response = serde_json::from_str(&input_string.to_string()).unwrap();
+    Ok(response)
 }
